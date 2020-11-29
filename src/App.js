0527974c2371f77115f14447e6f74a5d8dc977e5
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-11-29 18:33:49 +0500

    adding reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
 function App() {
+  const initialvalue =
+  {
+    count: 0
+  }
+  function reducer(state, action) {
+    switch (action) {
+      case 'incriment':
+        return { count: state.count + 1 }
+      case 'dicriment':
+        return { count: state.count - 1 }
+      case 'reset':
+        return { count: 0 }
+
+      default:
+        return state.count
+    }
+  }
+
+  const [state, dispatch] = React.useReducer(reducer, initialvalue)
+  console.log(state);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{state.count}</h3>
+      <br />
+      <button onClick={() => { dispatch('incriment') }}>incriment</button>
+      <button onClick={() => { dispatch('dicriment') }}>incriment</button>
+      <button onClick={() => { dispatch('reset') }}>Reset</button>
+
+
     </div>
   );
 }
